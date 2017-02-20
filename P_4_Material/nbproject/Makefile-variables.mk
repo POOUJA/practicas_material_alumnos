a7b3fbfc19b60aa709c1f98fa5245077b86dd917
algarcia <algarcia@ujaen.es>
2017-02-20 14:02:46 +0100

    Cambiada la plataforma de destino a GNU/Linux

--- P_4_Material/nbproject/Makefile-variables.mk
+++ P_4_Material/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-MacOSX
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=p_4_material
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/p_4_material
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/p_4_material
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=p4material.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/p4material.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/p4material.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
